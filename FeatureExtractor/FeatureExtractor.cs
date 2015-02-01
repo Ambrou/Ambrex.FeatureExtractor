6b8ea6cb0746db725d546cc0e7d765038ded4954
Ambrou <apetitgenet@free.fr>
2015-02-01 21:00:22 +0100

    début du premier scénario

---
+++ FeatureExtractor/FeatureExtractor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FeatureExtractor
+{
+    public class Extractor
+    {
+        public void extract()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void getRequirement(string requirementID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setRequirements(Dictionary<string, string> requirements)
+        {
+            m_Requirements = requirements;
+        }
+
+        private Dictionary<string, string> m_Requirements;
+    }
+}
