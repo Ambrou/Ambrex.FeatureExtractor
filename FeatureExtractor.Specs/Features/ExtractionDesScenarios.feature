cc3ab7606e2b2cf8ab054441d610e72ca02cf33d
Ambrou <apetitgenet@free.fr>
2015-02-03 19:43:24 +0100

    Ajout de fichier manquants

---
+++ FeatureExtractor.Specs/Features/ExtractionDesScenarios.feature
@@ -0,0 +1,16 @@
+﻿Feature: ExtractionDesScenarios
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Extraction de scénario simple
+	Given les exigences suivantes:
+			| ReqID   | TEXTE                                                                                                                                                                                                                        |   
+			| ESD_044 | "  Contexte: Étant donné un interpréteur de script Scénario: Changement de table Étant donné un matériel Et la configuration définissant la table 7 Lorsque j'appelle le mot clef changeTable 7 Alors le script retourne OK" |
+	When j'extrais les scénarios
+	Then le besoin ESD_044 existe
+		And Et il contient le scénario:
+		    | titre                            | scénario                                                                                                                                     |
+			|  "Scénario: Changement de table" | "Étant donné un matériel Et la configuration définissant la table 7 Lorsque j'appelle le mot clef changeTable 7 Alors le script retourne OK" |
+		And Et il contient le contexte "Soit un interpreteur de script"
---
+++ FeatureExtractor.Specs/Sources/ExtractionDesScenariosSteps.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+
+namespace FeatureExtractor.Specs.Sources
+{
+    [Binding]
+    public class ExtractionDesScenariosSteps
+    {
+        [Given(@"les exigences suivantes:")]
+        public void GivenLesExigencesSuivantes(Table table)
+        {
+            var requirements = new Dictionary<string, string>();
+
+            foreach (var row in table.Rows)
+            {
+                requirements[row["ReqID"]] = row["TEXTE"];
+            }
+            extractor.setRequirements(requirements);
+        }
+
+        [When(@"j'extrais les scénarios")]
+        public void WhenJExtraisLesScenarios()
+        {
+            extractor.extract();
+        }
+
+        [Then(@"le besoin (.*) existe")]
+        public void ThenLeBesoin_Existe(string strRequirementID)
+        {
+            requirment = extractor.getRequirement(strRequirementID);
+            Assert.Equals(strRequirementID, requirement.Id());  
+        }
+
+        [Then(@"Et il contient le scénario:")]
+        public void ThenEtIlContientLeScenario(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Et il contient le contexte ""(.*)""")]
+        public void ThenEtIlContientLeContexte(string strContexte)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+
+
+        Extractor extractor = new Extractor();
+        Requirement requirment;
+    }
+}
